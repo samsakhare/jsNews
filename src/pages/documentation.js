3eab485167ff069740e3d04a0d21b39bdd0a3a85
samsakhare <samsakhare@gmail.com>
2021-04-11 13:14:30 +0530

    Added documentation(inprogres)

--- src/pages/documentation.js
+++ src/pages/documentation.js
@@ -1,6 +1,12 @@
 import React from "react";
 const Documentation = () => {
-  return <h1>We are in the documentation section of the demo</h1>;
+  return (
+    <iframe
+      className="m-auto"
+      style={{ width: "70%", height: "90vh" }}
+      src="https://docs.google.com/document/d/e/2PACX-1vR7d7nZhn3E78AqiapWNuuMXekJ2rSW141rkWuiKw9qxpGOKAbVk1XG836Uc2Yd6kXInFcHQPHuH6f1/pub?embedded=true"
+    ></iframe>
+  );
 };
 
 export default Documentation;
